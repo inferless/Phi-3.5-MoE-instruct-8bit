d18554310bfb56accb9089969776a84d9ca539b2
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-09-26 05:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from mlx_lm import load, generate
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "microsoft/Phi-3.5-MoE-instruct"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_id, 
-            device_map="cuda",  
-            torch_dtype="auto",  
-            trust_remote_code=True
-        )
-        self.pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
+        self.model, self.tokenizer model, tokenizer = load("mlx-community/Phi-3.5-MoE-instruct-8bit")
+         
 
     def infer(self, inputs):
-        prompt = inputs["prompt"]
-        messages = [
-            {"role": "system", "content": "You are a helpful AI assistant."},
-            {"role": "user", "content": prompt},
-        ]
-        generation_args = { 
-            "max_new_tokens": 500, 
-            "return_full_text": False, 
-            "temperature": 0.0, 
-            "do_sample": False, 
-        }
-        out = self.pipe(messages, **generation_args)
-        generated_text = out[0]["generated_text"]
-        return {'generated_result': generated_text}
+        response = generate(model, tokenizer, prompt="hello", verbose=True)
+        return {'generated_result': response }
 
     def finalize(self):
         pass
